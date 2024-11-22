a892d05d3d4c8c60afc4c96c366d74bb2a8b5404
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-11-22 05:32:50 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main>
+      <h1 className="w-fit rounded bg-slate-500 p-2 text-2xl text-pink-300">
+        Hello, Vite + React + TypeScript!
+      </h1>
+    </main>
+  );
+}
