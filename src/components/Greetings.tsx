91060eb4bc0da1b74c72e6f371abc0b24d87eb11
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-12-04 07:40:34 +0300

    removed

--- src/components/Greetings.tsx
+++
@@ -1,9 +0,0 @@
-const Greetings: React.FC = () => {
-  return (
-    <h1 className="w-fit rounded p-2 text-2xl text-red-300">
-      Hello, Vite + React + TypeScript!
-    </h1>
-  );
-};
-
-export default Greetings;
