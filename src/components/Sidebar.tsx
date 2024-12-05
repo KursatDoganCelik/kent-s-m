84bdf545442ddd245050666b8485bd04058d2474
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-12-05 11:32:31 +0300

    selected index bg-color added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ChevronLeft, Home, ShoppingBag } from "lucide-react";
 
 const Sidebar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(true);
+  const location = useLocation();
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
@@ -26,13 +27,11 @@ const Sidebar: React.FC = () => {
 
   return (
     <div
-      className={`flex transition-all duration-300 ${
-        isOpen ? "w-56" : "w-0"
-      } bg-gray-600 text-white`}
+      className={`flex bg-slate-600 text-white transition-all duration-300 ${isOpen ? "w-56" : "w-0"}`}
     >
-      <aside className="w-52 bg-gray-600 p-4 text-white">
+      <aside className="w-52 p-4 text-white">
         <button
-          className={`absolute top-4 rounded-l-full bg-gray-700 p-2 transition-all duration-300 ${
+          className={`absolute top-4 rounded-l-full bg-slate-700 p-2 transition-all duration-300 ${
             isOpen ? "left-[184px] rotate-0" : "left-[-10px] rotate-180"
           }`}
           onClick={toggleSidebar}
@@ -45,7 +44,9 @@ const Sidebar: React.FC = () => {
           <li>
             <Link
               to="/"
-              className="flex items-center rounded p-2 text-lg transition duration-200 hover:bg-gray-700"
+              className={`flex items-center rounded p-2 text-lg transition duration-200 hover:bg-slate-700 ${
+                location.pathname === "/" ? "bg-slate-700" : ""
+              }`}
             >
               <Home size={20} className="mr-2" />
               Home
@@ -54,7 +55,9 @@ const Sidebar: React.FC = () => {
           <li>
             <Link
               to="/products"
-              className="flex items-center rounded p-2 text-lg transition duration-200 hover:bg-gray-700"
+              className={`flex items-center rounded p-2 text-lg transition duration-200 hover:bg-slate-700 ${
+                location.pathname === "/products" ? "bg-slate-700" : ""
+              }`}
             >
               <ShoppingBag size={20} className="mr-2" />
               Products
